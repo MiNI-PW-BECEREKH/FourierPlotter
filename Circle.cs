33291e5981632a7adf078c8a9da674eeb4a12726
sbecerek <suheybbecerek@gmail.com>
2021-05-17 16:09:34 +0300

    timer complete

---
+++ Circle.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Ink;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using WPFLAB.Annotations;
+
+namespace WPFLAB
+{
+
+    public class Circle : UIElement, INotifyPropertyChanged
+    {
+        
+        public Ellipse ellipse = new Ellipse
+        {
+            Stroke = new SolidColorBrush(Colors.Black),
+            StrokeThickness = 2
+        };
+
+        private int radius;
+
+        public int Radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int HorizontalRight { get; set; }
+        public int Frequency { get; set; }
+        public Point Position { get; set; }
+
+        public Circle()
+        {
+            PropertyChanged += Circle_PropertyChanged;
+        }
+
+        private void Circle_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Radius":
+                {
+                    ellipse.Width = Radius;
+                    ellipse.Height = Radius;
+
+                } break;
+
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+    }
+    
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +14,7 @@ using System.Windows.Documents;
 using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
@@ -22,32 +26,68 @@ namespace WPFLAB
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window 
     {
-        private DispatcherTimer timer = new DispatcherTimer();
+        //To Remove Close Button -- WPF doesn't have such built in functionality
+        // Prep stuff needed to remove close button on window
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+
+        private DispatcherTimer timer = new DispatcherTimer(DispatcherPriority.Render);
+        private Stopwatch stopwatch = new Stopwatch();
+        private bool IsPaused { get; set; } = false;
+
         public MainWindow()
         {
             InitializeComponent();
-            this.DataContext = new List<RowEntry>{new RowEntry{A = 1,B=1,C=1}};
-            timer.Interval = new TimeSpan(0, 0, 0,0,1);
-            timer.Tick += new EventHandler(Tick);
             
         }
 
 
-
-        public void Tick(object sender, EventArgs e)
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
         {
-            if (ProgressBar.Value < 100)
+            //To Remove Close Button -- WPF doesn't have such built in functionality
+            // Code to remove close box from window
+            var hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+
+            this.DataContext = new ObservableCollection<Circle> { new Circle { Radius = 200, Frequency = 1 } };
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Tick;
+            
+            //this.DataContext = Circles;
+            ((INotifyCollectionChanged)circlesDataGrid.Items).CollectionChanged += Circles_CollectionChanged;
+
+            
+
+        }
+
+        private void Circles_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            //DrawCircles();
+        }
+
+        public async void Tick(object sender, EventArgs e)
+        {
+            if (stopwatch.Elapsed.TotalSeconds < 10)
             {
-                ProgressBar.Value += ProgressBar.ActualWidth/10000;
+                ProgressBar.Value = stopwatch.ElapsedMilliseconds;
             }
             else
             {
                 timer.Stop();
-                Debug.WriteLine("TIMER STOPPED");
-                
+                stopwatch.Stop();
+                stopwatch.Reset();
+                DrawCircles();
+
             }
+
         }
         private void ExitMenuOption_Clicked(object sender, RoutedEventArgs e)
         {
@@ -57,17 +97,66 @@ namespace WPFLAB
         private void Start_Clicked(object sender, RoutedEventArgs e)
         {
             timer.Start();
+            stopwatch.Start();
         }
 
         private void Pause_Clicked(object sender, RoutedEventArgs e)
         {
+            stopwatch.Stop();
             timer.Stop();
         }
 
         private void Reset_Clicked(object sender, RoutedEventArgs e)
         {
             ProgressBar.Value = 0;
+            stopwatch.Reset();
             timer.Stop();
+            Plotter.Children.Clear();
+        }
+
+        private void MenuItem_OnChecked(object sender, RoutedEventArgs e)
+        {
+            if(circlesDataGrid != null)
+            foreach (var item in circlesDataGrid.Items.SourceCollection)
+            {
+                Circle c = item as Circle;
+                c.ellipse.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void MenuItem_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (circlesDataGrid != null)
+            foreach (var item in circlesDataGrid.Items.SourceCollection)
+            {
+                Circle c = item as Circle;
+                c.ellipse.Visibility = Visibility.Hidden;
+            }
+        }
+
+
+
+        private void DrawCircles()
+        {
+            var previousCircle = (Circle)null;
+            foreach (var item in circlesDataGrid.Items.SourceCollection)
+            {
+                var circleItem = item as Circle;
+                if (previousCircle != null)
+                {
+
+                    circleItem.HorizontalRight = (int)(previousCircle.HorizontalRight + circleItem.ellipse.Width / 2);
+                    Canvas.SetLeft(circleItem.ellipse, previousCircle.HorizontalRight - circleItem.ellipse.Width / 2);
+                }
+                else
+                {
+                    circleItem.HorizontalRight = (int)(Plotter.ActualWidth / 2 + circleItem.ellipse.Width / 2);
+                    Canvas.SetLeft(circleItem.ellipse, Plotter.ActualWidth / 2 - circleItem.ellipse.Width / 2);
+                }
+                Canvas.SetTop(circleItem.ellipse, Plotter.ActualHeight / 2 - circleItem.ellipse.Height / 2);
+                previousCircle = circleItem;
+                Plotter.Children.Add(circleItem.ellipse);
+            }
         }
 
     }
